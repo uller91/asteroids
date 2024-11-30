81361d4e7e32fea03bfcb8a5e8d6d96b6688ca80
uller91 <girnikis@gmail.com>
2024-11-29 19:30:00 -0800

    pygame loop and black screen added

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pygame
-
-
-
-
-
-
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
 def main():
     print("Starting asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:     #infinite loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))      #fill screen with black
+        pygame.display.flip()       #refresh screen
 
 
 
