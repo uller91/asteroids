a335a5f9c69a209adac6e1fb406cb4feec8cefef
uller91 <girnikis@gmail.com>
2024-11-29 20:12:49 -0800

    added a ship class

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:     #infinite loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))      #fill screen with black
+            
+        screen.fill('black')      #fill screen with black
+        ship.draw(screen)
         pygame.display.flip()       #refresh screen
-        dt += clock.tick(60)/1000             #60 FPS
+        dt = clock.tick(60)/1000             #60 FPS in s
         #print(dt)
+
 
 
 if __name__ == "__main__":
