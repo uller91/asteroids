d94c4007be1fdd9a8bd43c70fa3380fff9f6e87b
uller91 <girnikis@gmail.com>
2024-11-29 19:43:26 -0800

    FPS timer added

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:     #infinite loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))      #fill screen with black
         pygame.display.flip()       #refresh screen
-
+        dt += clock.tick(60)/1000             #60 FPS
+        #print(dt)
 
 
 if __name__ == "__main__":
